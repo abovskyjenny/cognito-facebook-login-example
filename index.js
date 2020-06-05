ba21ea239836ab90c775c3b9cd4ef43fe797a226
Jenny Abovsky <jenny.abovsky@naturalint.com>
2020-06-05 15:55:59 +0300

    update

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 const express = require('express')
 const path = require('path')
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
 // hide powered by express
 app.disable('x-powered-by');
 
 // Serving static files
-app.use('/test', express.static(path.resolve(__dirname, 'test')));
-app.use(express.static('test'));
+app.use('/public', express.static(path.resolve(__dirname, 'test')));
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     console.log('__dirname ', __dirname)
-    res.sendFile(__dirname + '/test/facebook.html');
+    res.sendFile(__dirname + '/public/facebook.html');
 });
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
